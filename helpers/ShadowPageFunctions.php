e86185cdf1c70ec37b459b1b8f32541e7ef665b9
Matti Lassila <matti.lassila@gmail.com>
2014-03-08 13:45:14 +0200

    Use proper item section for download links. Only display links for files not displayed in the gallery.

--- helpers/ShadowPageFunctions.php
+++ helpers/ShadowPageFunctions.php
@@ -12,18 +12,30 @@ function shadowpage_public_items_shadowpage_gallery()
     echo '<div id="itemfiles" class="element">';
     $fr_link_string = array();
     foreach($files as $file):
-      if (metadata($item, 'has thumbnail')){
+      if (metadata($item, 'has thumbnail') && ($file['mime_type'] != 'text/plain' && $file['mime_type'] !='application/epub+zip' && $file['mime_type'] !='application/zip')){
         echo '<div class="shadowpage-thumb">'.file_markup($file, array('linkAttributes'=>array('rel'=>'shadowbox[gal1]'))).'</div>';
         $fr_link_string[] = '<div class="download-full-res"><a href="'. $basePath . '/files/original/'.$file['filename'].'" title="Download ' . $file['original_filename'] . '">' . $file['original_filename'] . '</a></div>';
       }
     endforeach;
  
     echo '</div>';
-    echo '<h3>&darr;'.__('File Download Links').'&darr;</h3>';
-    foreach($fr_link_string as $links):
-      echo $links;
-    endforeach;
-  
-  
-  
   }
+
+/**
+ * Return HTML for all files assigned to an item.
+ * 
+ * @package Omeka\Function\View\Item
+ * @uses file_markup()
+ * @param array $options
+ * @param array $wrapperAttributes
+ * @param Item|null $item Check for this specific item record (current item if null).
+ * @return string HTML
+ */
+function shadowpage_files_for_item($options = array(), $wrapperAttributes = array('class'=>'item-file'), $item = null)
+{
+    if (!$item) {
+        $item = get_current_record('item');
+    }
+    return file_markup($item->Files, $options, $wrapperAttributes);
+}
+
